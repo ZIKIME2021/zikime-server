8dae3be7727b3a578d3f34f9a06d8c8964ddf3a4
sooyeon-kr <sooyeon7027@naver.com>
2021-09-04 13:27:40 +0900

    Remove duplicated module

--- front/src/pages/WatchPage.js
+++ front/src/pages/WatchPage.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import ProtegeInfoList from '../components/play/ProtegeInfoList';
 
-const WatchPage = () => {
-    return <div>피보호자 정보보기 페이지</div>;
-};
+const WatchWrapper = styled.div`
+
+`;
+
+class WatchPage extends Component {
+
+    id = 2;
+    state = {
+        protege : [
+            {
+                id : 0,
+                name: '홍길동',
+                phone: '010 - 1234- 5678',
+            },
+            {
+                id : 1,
+                name: '박서준',
+                phone: '010 - 4321- 8765',
+            },
+        ]
+    }
+
+    render() {
+        const divStyle = {
+            display: 'block',
+            margin: '0 auto',
+            align: 'center',
+            width: '30vw',
+        }
+
+        return (
+            <WatchWrapper style={divStyle}>
+                <div style={{margin:'2px 2px'}}>등록된 피보호자</div>
+                <ProtegeInfoList 
+                    data={this.state.protege}/>
+            </WatchWrapper>
+        );
+    }
+}
 
 export default WatchPage;
